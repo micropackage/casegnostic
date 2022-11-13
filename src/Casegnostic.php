d251a965af1cad1655ff69614de2f8c8575cbc25
Krystian Wojciechowski <wojciechowskik1010@gmail.com>
2022-11-13 20:10:58 +0100

    package init

---
+++ src/Casegnostic.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Casegnostic
+ *
+ * @package micropackage/casegnostic
+ */
+
+namespace Micropackage\Casegnostic;
+
+/**
+ * Casegnostic trait
+ */
+trait Casegnostic
+{
+}
