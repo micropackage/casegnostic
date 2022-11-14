ef967d57478e82e60ec2b38307cdfaf727322347
Krystian Wojciechowski <wojciechowskik1010@gmail.com>
2022-11-14 17:14:05 +0100

    Casegnostic logic

--- src/Casegnostic.php
+++ src/Casegnostic.php
@@ -10,9 +10,78 @@ declare(strict_types=1);
 
 namespace Micropackage\Casegnostic;
 
+use Micropackage\Casegnostic\Helpers\CaseHelper;
+
 /**
  * Casegnostic trait
  */
 trait Casegnostic
 {
+    /**
+     * @param string $name
+     * @return mixed
+     * @throws \Exception
+     */
+    public function __get( string $name)
+    {
+        if (CaseHelper::isSnake($name)) {
+            if (property_exists($this, CaseHelper::toCamel($name))) {
+                return $this->{CaseHelper::toCamel($name)};
+            }
+        }
+
+        if (CaseHelper::isCamel($name)) {
+            if (property_exists($this, CaseHelper::toSnake($name))) {
+                return $this->{CaseHelper::toSnake($name)};
+            }
+        }
+
+        throw new \Exception('property does not exist');
+    }
+
+    /**
+     * @param string $name
+     * @param array<mixed> $arguments
+     * @return mixed
+     * @throws \Exception
+     */
+    public function __call( string $name, array $arguments)
+    {
+        if (CaseHelper::isSnake($name)) {
+            if (method_exists($this, CaseHelper::toCamel($name))) {
+                return $this->{CaseHelper::toCamel($name)}(...$arguments);
+            }
+        }
+
+        if (CaseHelper::isCamel($name)) {
+            if (method_exists($this, CaseHelper::toSnake($name))) {
+                return $this->{CaseHelper::toSnake($name)}(...$arguments);
+            }
+        }
+
+        throw new \http\Exception\BadMethodCallException('method does not exist');
+    }
+
+    /**
+     * @param string $name
+     * @param array<mixed> $arguments
+     * @return mixed
+     * @throws \Exception
+     */
+    public static function __callStatic( string $name, array $arguments)
+    {
+        if (CaseHelper::isSnake($name)) {
+            if (method_exists(self::class, CaseHelper::toCamel($name))) {
+                return self::{CaseHelper::toCamel($name)}(...$arguments);
+            }
+        }
+
+        if (CaseHelper::isCamel($name)) {
+            if (method_exists(self::class, CaseHelper::toSnake($name))) {
+                return self::{CaseHelper::toSnake($name)}(...$arguments);
+            }
+        }
+
+        throw new \http\Exception\BadMethodCallException('static method does not exist');
+    }
 }
---
+++ tests/unit/CasegnosticTest.php
@@ -0,0 +1,54 @@
+<?php
+
+$object = new class {
+	use \Micropackage\Casegnostic\Casegnostic;
+
+	public int $snake_case_property = 1;
+	public int $camelCaseProperty = 1;
+
+	public function snake_case_method(): int { return 1; }
+	public function camelCaseMethod(): int { return 1; }
+	public static function snake_case_static_method(): int { return 1; }
+	public static function camelCaseStaticMethod(): int { return 1; }
+
+	public function snake_case_method_one_arg(string $a): string { return $a; }
+	public function camelCaseMethodOneArg(string $a): string { return $a; }
+	public static function snake_case_static_method_one_arg(string $a): string { return $a; }
+	public static function camelCaseStaticMethodOneArg(string $a): string { return $a; }
+
+	public function snake_case_method_two_arg(string $a, string $b): string { return $a . $b; }
+	public function camelCaseMethodTwoArg(string $a, string $b): string { return $a . $b; }
+	public static function snake_case_static_method_two_arg(string $a, string $b): string { return $a . $b; }
+	public static function camelCaseStaticMethodTwoArg(string $a, string $b): string { return $a . $b; }
+};
+
+test('call to undefined property throws exception', function() use ($object) {
+	return $object->undefined_property;
+})->throws(Exception::class);
+
+test('call to undefined method throws exception', function() use ($object) {
+	return $object->undefinedMethod();
+})->throws(\http\Exception\BadMethodCallException::class);
+
+test('call to undefined static method throws exception', function() use ($object) {
+	return $object::undefinedStaticMethod();
+})->throws(\http\Exception\BadMethodCallException::class);
+
+test('test if different cases fit each other', function($first, $second) {
+	expect($first)->toBe($second);
+})->with([
+	'snake_case property as camelcase' => [$object->snakeCaseProperty, $object->snake_case_property],
+	'camelCase property as snake_case' => [$object->camel_case_property, $object->camelCaseProperty],
+	'snake_case method as camelCase' => [$object->snakeCaseMethod(), $object->snake_case_method()],
+	'camelCase method as snake_case' => [$object->camel_case_method(), $object->camelCaseMethod()],
+	'snake_case static method as camelCase' => [$object::snakeCaseStaticMethod(), $object::snake_case_static_method()],
+	'camelCase static method as snake_case' => [$object::camelCaseStaticMethod(), $object::camel_case_static_method()],
+	'snake_case method as camelCase, one arg' => [$object->snakeCaseMethodOneArg('test'), $object->snake_case_method_one_arg('test')],
+	'camelCase method as snake_case, one arg' => [$object->camelCaseMethodOneArg('test'), $object->camel_case_method_one_arg('test')],
+	'snake_case static method as camelCase, one arg' => [$object::snakeCaseStaticMethodOneArg('test'), $object::snake_case_static_method_one_arg('test')],
+	'camelCase static method as snake_case, one arg' => [$object::camelCaseStaticMethodOneArg('test'), $object::camel_case_static_method_one_arg('test')],
+	'snake_case method as camelCase, two arg' => [$object->snakeCaseMethodTwoArg('test', ' passed'), $object->snake_case_method_two_arg('test', ' passed')],
+	'camelCase method as snake_case, two arg' => [$object->camelCaseMethodTwoArg('test', ' passed'), $object->camel_case_method_two_arg('test', ' passed')],
+	'snake_case static method as camelCase, two arg' => [$object::snakeCaseStaticMethodTwoArg('test', ' passed'), $object::snake_case_static_method_two_arg('test', ' passed')],
+	'camelCase static method as snake_case, two arg' => [$object::camelCaseStaticMethodTwoArg('test', ' passed'), $object::camel_case_static_method_two_arg('test', ' passed')],
+]);
